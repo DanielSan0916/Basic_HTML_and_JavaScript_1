e15fc0a9d8dd6ce2af41be025f1e92b3318a2878
DanielSan0916 <jessan0916@gmail.com>
2025-03-03 12:54:17 -0700

    Separated HTML and JavaScript

--- Basic_JavaScript_1.js
+++ Basic_JavaScript_1.js
@@ -1,1 +1,8 @@
 alert ("Welcome to Shutter Stories!");
+
+function My_First_Function() {
+		var String = "Kissme, I'm Irish!";
+		var result = String.fontcolor("green");
+		document.getElementById("Irish").innerHTML =
+		result;
+	}
