ec803c170e4e9f54054f01e254dd01434bca36dd
DanielSan0916 <jessan0916@gmail.com>
2025-03-04 20:27:01 -0700

    Cleaned up JavaScript source codes
    Cleaned up JavaScript source codes and HTML code

--- Basic_JavaScript_1.js
+++ Basic_JavaScript_1.js
@@ -1,8 +1,1 @@
 alert ("Welcome to Shutter Stories!");
-
-function My_First_Function() {
-		var String = "Kissme, I'm Irish!";
-		var result = String.fontcolor("green");
-		document.getElementById("Irish").innerHTML =
-		result;
-	}
---
+++ Basic_JavaScript_2.js
@@ -0,0 +1,6 @@
+function My_First_Function() {
+		var String = "Kissme, I'm Irish!";
+		var result = String.fontcolor("green");
+		document.getElementById("Irish").innerHTML =
+		result;
+	}
